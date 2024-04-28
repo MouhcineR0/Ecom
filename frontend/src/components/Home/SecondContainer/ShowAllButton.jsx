882190107c393fb0f4341f0a046631c1d29c366a
sadbox <rachidmouhcine88@gmail.com>
2024-04-28 19:11:44 +0100

    Flash Design Completed

--- frontend/src/components/Home/SecondContainer/ShowAllButton.jsx
+++ frontend/src/components/Home/SecondContainer/ShowAllButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function ShowAllButton() {
     return (
         <div className='w-full mx-auto flex justify-center mt-[50px]'>
-            <Link className='bg-primary text-white px-8 py-3 md:text-[17px] text-[15px] rounded-md font-poppins hover:text-primary hover:bg-white border hover:border-primary'>
+            <Link className='bg-primary text-white px-[48px] py-[16px] md:text-[16px] text-[15px] rounded-[4px] font-poppins font-medium hover:text-primary hover:bg-white border hover:border-primary'>
                 View All Products
             </Link>
         </div>
--- frontend/src/components/Home/SecondContainer/index.jsx
+++ frontend/src/components/Home/SecondContainer/index.jsx
@@ -8,11 +8,14 @@ function index() {
     // handling the second swiper #offers
     const [swiper, setSwiper] = useState(null);
 
+    const TargetDate = '2024-04-31T23:59:59';
+
     return (
         <>
-            <FlashContainer targetDate={'2024-04-31T23:59:59'} setSwiper={setSwiper} swiper={swiper} />
+            <FlashContainer targetDate={TargetDate} setSwiper={setSwiper} swiper={swiper} />
             <FlashProducts setSwiper={setSwiper} swiper={swiper} />
             <ShowAllButton />
+            <hr className='w-[90%] mx-auto mt-[50px] border-[1px] border-[#e5e5e5]' />
         </>
     );
 }
